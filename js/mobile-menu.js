5112e86775b62ec07c2751d3c22b404a6a2c7218
Uziel <pessoal.ziera@gmail.com>
2025-02-10 19:36:48 -0300

    adicionei menu para mobile

---
+++ js/mobile-menu.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName("hamburguer")[0].addEventListener("click", evt =>
+{
+    evt.target.classList.toggle("open");
+});
--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@ const VERSION = "v0.0.1";
 const CACHE_NAME = `getz-${VERSION}`;
 
 const PRECACHE_ASSETS =
-[
-    "/css/",
-    "/fonts/",
-    "/js/",
-    "/images/"
-];
+    [
+        "/GETZ/fonts/",
+        "/GETZ/images/"
+        // "/fonts/",
+        // "/images/"
+    ];
 
 self.addEventListener("install", evt => 
 {
@@ -21,42 +21,42 @@ self.addEventListener("install", evt =>
 self.addEventListener("fetch", async evt => 
 {
     evt.respondWith((async () => 
+    {
+        const CACHE = await caches.open(CACHE_NAME);
+
+        let CACHED_RESPONSE;
+        switch (new URL(evt.request.url).pathname)
         {
-            const CACHE = await caches.open(CACHE_NAME);
-            
-            let CACHED_RESPONSE;
-            switch (new URL(evt.request.url).pathname)
+            case "/GETZ/":
+                CACHED_RESPONSE = await CACHE.match(evt.request.url + "index.html");
+                break;
+            case "/GETZ/sobre":
+                CACHED_RESPONSE = await CACHE.match(evt.request.url + "sobre.html");
+                break;
+            case "/GETZ/membros":
+                CACHED_RESPONSE = await CACHE.match(evt.request.url + "membros.html");
+                break;
+            case "/GETZ/ferramentas":
+                CACHED_RESPONSE = await CACHE.match(evt.request.url + "ferramentas.html");
+                break;
+            default:
+                CACHED_RESPONSE = await CACHE.match(evt.request);
+                break;
+        }
+
+        if (CACHED_RESPONSE !== undefined)
+        {
+            fetch(evt.request).then(res => 
             {
-                case "/GETZ/":
-                    CACHED_RESPONSE = await CACHE.match(evt.request.url + "index.html");
-                    break;
-                case "/GETZ/sobre":
-                    CACHED_RESPONSE = await CACHE.match(evt.request.url + "sobre.html");
-                    break;
-                case "/GETZ/membros":
-                    CACHED_RESPONSE = await CACHE.match(evt.request.url + "membros.html");
-                    break;
-                case "/GETZ/ferramentas":
-                    CACHED_RESPONSE = await CACHE.match(evt.request.url + "ferramentas.html");
-                    break;
-                default:
-                    CACHED_RESPONSE = await CACHE.match(evt.request);
-                    break;
-            }
-            
-            if (CACHED_RESPONSE !== undefined)
-            {
-                fetch(evt.request).then( res => 
-                {
-                    CACHE.put(evt.request, res.clone());
-                });
-                
-                return CACHED_RESPONSE;
-            }
-            
-            const NETWORK_RESPONSE = await fetch(evt.request);
-            CACHE.put(evt.request, NETWORK_RESPONSE.clone());
-            
-            return NETWORK_RESPONSE;
-        })());
+                CACHE.put(evt.request, res.clone());
+            });
+
+            return CACHED_RESPONSE;
+        }
+
+        const NETWORK_RESPONSE = await fetch(evt.request);
+        CACHE.put(evt.request, NETWORK_RESPONSE.clone());
+
+        return NETWORK_RESPONSE;
+    })());
 });
